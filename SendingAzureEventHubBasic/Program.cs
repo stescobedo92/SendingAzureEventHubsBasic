d4e8b9df5df81a350f67fb5d4c472bc46dd7ea61
stescobedo92 <stescobedo.31@gmail.com>
2023-04-19 10:13:28 -0600

    Create producer and send events

--- SendingAzureEventHubBasic/Program.cs
+++ SendingAzureEventHubBasic/Program.cs
@@ -1,4 +1,7 @@
-﻿namespace SendingAzureEventHubBasic
+﻿using Azure.Messaging.EventHubs;
+using Azure.Messaging.EventHubs.Producer;
+
+namespace SendingAzureEventHubBasic
 {
     internal class Program
     {
@@ -9,7 +12,16 @@
             string namespaceConnectionString = "Endpoint=sb://resources-events.servicebus.windows.net/;SharedAccessKeyName=sendandreceive;SharedAccessKey=B87i7DCK7Wk7UL52UQ9MgKdLI/GOPvb3t+AEhHRa0Hk=;EntityPath=demoevnethub";
             string eventHubName = "demoevnethub";
 
+            EventHubProducerClient producer = new EventHubProducerClient(namespaceConnectionString, eventHubName);
+            List<EventData> events = new List<EventData>();
 
+            for (int i = 0; i < 10; i++) 
+            {
+                events.Add(new EventData($"This is event: {i}"));
+            }
+
+            await producer.SendAsync(events);
+            Console.WriteLine("Sent the events");
         }
     }
 }
